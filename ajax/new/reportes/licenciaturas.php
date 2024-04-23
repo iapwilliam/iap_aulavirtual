ba822c09c518daef23b46a84367f7618bd18c233
William Ramírez <wramirez@iapchiapas.edu.mx>
2024-04-23 11:55:58 -0600

    Reporte de inscritos a las licenciaturas. Correción error y añadido estilos a las hojas

--- ajax/new/reportes/licenciaturas.php
+++ ajax/new/reportes/licenciaturas.php
@@ -1,21 +1,23 @@
 <?php
-include_once('../../../init.php');
+include_once('../../../initPdf.php');
 include_once('../../../config.php');
 include_once(DOC_ROOT . '/libraries.php');
 session_start();
+
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
+
 $licenciatura = $_GET['licenciatura'];
-if ($licenciatura == 0) {  
-    $licenciaturas = $course->getCourses("AND courseId > 2"); 
-}else{
-    $licenciaturas = $course->getCourses("AND courseId = $licenciatura"); 
+if ($licenciatura == 0) {
+    $licenciaturas = $course->getCourses("AND courseId > 2");
+} else {
+    $licenciaturas = $course->getCourses("AND courseId = $licenciatura");
 }
 foreach ($licenciaturas as $key => $licenciatura) {
     $licenciaturas[$key]['registrados'] = $course->getStudents("AND user_subject.courseId = {$licenciatura['courseId']}");
-} 
+}
 $spreadsheet = new Spreadsheet();
 $spreadsheet->getProperties()->setCreator('William Ramírez')
     ->setLastModifiedBy('William Ramírez')
@@ -24,10 +26,10 @@ $spreadsheet->getProperties()->setCreator('William Ramírez')
     ->setDescription('Reporte de inscritos a licenciaturas')
     ->setKeywords('Alumnos, Reportes, Licenciaturas, Inscritos')
     ->setCategory('Reportes');
-foreach ($licenciaturas as $item) {
-    $spreadsheet->createSheet();
-    $spreadsheet->getDefaultStyle()->getFont()->setName('Times New Roman');
+$ultimo_indice = count($licenciaturas) - 1;
+foreach ($licenciaturas as $key => $item) {
     $sheet = $spreadsheet->getActiveSheet();
+    $sheet->setTitle($item['subject_name']);
     $sheet->setCellValue('A1', 'Usuario');
     $sheet->setCellValue('B1', 'Contraseña');
     $sheet->setCellValue('C1', 'Nombre');
@@ -37,9 +39,18 @@ foreach ($licenciaturas as $item) {
     $sheet->setCellValue('G1', 'Telefono');
     $sheet->setCellValue('H1', 'Lugar de trabajo');
     $sheet->setCellValue('I1', 'Puesto');
+    $sheet->getColumnDimension('A')->setAutoSize(true);
+    $sheet->getColumnDimension('B')->setAutoSize(true);
+    $sheet->getColumnDimension('C')->setAutoSize(true);
+    $sheet->getColumnDimension('D')->setAutoSize(true);
+    $sheet->getColumnDimension('E')->setAutoSize(true);
+    $sheet->getColumnDimension('F')->setAutoSize(true);
+    $sheet->getColumnDimension('G')->setAutoSize(true);
+    $sheet->getColumnDimension('H')->setAutoSize(true);
+    $sheet->getColumnDimension('I')->setAutoSize(true);
 
     $sheet->getStyle('A')->getAlignment()->setHorizontal('center')->setVertical('center');
-    $sheet->getStyle('A')->getFont()->setSize(14)->setBold(true);
+    $sheet->getStyle('A')->getFont()->setSize(14)->setBold(true); 
     $sheet->getStyle('B')->getAlignment()->setHorizontal('center')->setVertical('center');
     $sheet->getStyle('B')->getFont()->setSize(14)->setBold(true);
     $sheet->getStyle('C')->getAlignment()->setHorizontal('center')->setVertical('center');
@@ -58,22 +69,26 @@ foreach ($licenciaturas as $item) {
     $sheet->getStyle('I')->getFont()->setSize(14)->setBold(true);
 
     $students = $item['registrados'];
-    for ($i = 0; $i < (count($students)); $i++) {  
+
+    for ($i = 0; $i < (count($students)); $i++) {
         $sheet->setCellValue("A" . ($i + 2), $students[$i]['controlNumber']);
         $sheet->setCellValue("B" . ($i + 2), $students[$i]['password']);
         $sheet->setCellValue("C" . ($i + 2), mb_strtoupper($students[$i]['names']));
         $sheet->setCellValue("D" . ($i + 2), mb_strtoupper($students[$i]['lastNamePaterno']));
-        $sheet->setCellValue("E" . ($i + 2), mb_strtoupper($students[$i]['lastNameMaterno'])); 
+        $sheet->setCellValue("E" . ($i + 2), mb_strtoupper($students[$i]['lastNameMaterno']));
         $sheet->setCellValue("F" . ($i + 2), $students[$i]['email']);
         $sheet->setCellValue("G" . ($i + 2), $students[$i]['phone']);
         $sheet->setCellValue("H" . ($i + 2), $students[$i]['workplace']);
-        $sheet->setCellValue("I" . ($i + 2), $students[$i]['workplacePosition']); 
+        $sheet->setCellValue("I" . ($i + 2), $students[$i]['workplacePosition']);
     }
 
-    $sheet->getStyle("A2:I" . (count($students) + 1))->getAlignment()->setHorizontal('center')->setVertical('center')->setWrapText(true);
+    $sheet->getStyle("A1:I" . (count($students) + 1))->getAlignment()->setHorizontal('center')->setVertical('center')->setWrapText(true);
+    if ($ultimo_indice !== $key) {
+        $spreadsheet->createSheet();
+        $spreadsheet->setActiveSheetIndex($key + 1);
+    }
 }
-
-
+$spreadsheet->setActiveSheetIndex(0);
 $fileName = bin2hex(random_bytes(4));
 // Redirect output to a client’s web browser (Xls)
 header('Content-Type: application/vnd.ms-excel; charset=utf-8');
