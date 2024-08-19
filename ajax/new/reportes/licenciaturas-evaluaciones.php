7cb02271b601891163ba85c9cba313d3eff41293
William Ramírez <wramirez@iapchiapas.edu.mx>
2024-08-19 13:40:42 -0600

    Fix se considera el reporte de evaluaciones de las licenciaturas para varias currículas

--- ajax/new/reportes/licenciaturas-evaluaciones.php
+++ ajax/new/reportes/licenciaturas-evaluaciones.php
@@ -6,13 +6,26 @@ session_start();
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\IOFactory;
+
+$licenciatura = $_GET['licenciatura'];
+$grupo = $_GET['grupo'];
  
-$course->setCourseId($_GET['grupo']);
-$courseData = $course->getCourse();
-$headings = $course->getHeadersActivities("AND course_module.courseId = {$_GET['grupo']}");
-$students = $course->getStudents("AND user_subject.courseId = {$_GET['grupo']}");
+if ($licenciatura == 0) {
+    $licenciaturas = $course->getCourses("AND subject.tipo = 4");
+}elseif ($licenciatura != 0 && $grupo == 0) { 
+    $licenciaturas = $course->getCourses("AND subject.subjectId = $licenciatura");
+}else{
+    $licenciaturas = $course->getCourses("AND course.courseId = $grupo");
+}
+
+foreach ($licenciaturas as $key => $licenciatura) {
+    $licenciaturas[$key]['registrados'] = $course->getStudents("AND user_subject.courseId = {$licenciatura['courseId']}");
+}
+
+
+
 $spreadsheet = new Spreadsheet();
-$spreadsheet->getActiveSheet()->getDefaultColumnDimension()->setWidth(30);
+
 // Set document properties
 $spreadsheet->getProperties()->setCreator('William Ramírez')
     ->setLastModifiedBy('William Ramírez')
@@ -23,40 +36,54 @@ $spreadsheet->getProperties()->setCreator('William Ramírez')
     ->setCategory('Cursos');
 $sheet = $spreadsheet->getActiveSheet();
 
-$sheet->setCellValue('A1', 'Usuario');
-$sheet->setCellValue('B1', 'Nombre');
-$sheet->setCellValue('C1', 'Apellido Paterno');
-$sheet->setCellValue('D1', 'Apellido Materno'); 
-$auxHeading = "E";
+$ultimo_indice = count($licenciaturas) - 1;
+foreach ($licenciaturas as $key => $item) {
+    $course->setCourseId($item['courseId']);
+    $courseData = $course->getCourse();
+    $headings = $course->getHeadersActivities("AND course_module.courseId = {$item['courseId']}");
+    $students = $course->getStudents("AND user_subject.courseId = {$item['courseId']}");
+    $sheet = $spreadsheet->getActiveSheet(); 
+    $sheet->getDefaultColumnDimension()->setWidth(30);
+    $sheet->setTitle($item['subject_name']. " GRUPO ". $item['group']);
+    $sheet->setCellValue('A1', 'Usuario');
+    $sheet->setCellValue('B1', 'Nombre');
+    $sheet->setCellValue('C1', 'Apellido Paterno');
+    $sheet->setCellValue('D1', 'Apellido Materno'); 
+    $auxHeading = "E";
 
-foreach ($headings as $item) {
-    $sheet->setCellValue("{$auxHeading}1", $item['resumen']);
-    $auxHeading++;
-} 
+    foreach ($headings as $item) {
+        $sheet->setCellValue("{$auxHeading}1", $item['resumen']);
+        $auxHeading++;
+    } 
 
-$auxRow = 2;
-for ($i = 0; $i < (count($students)); $i++) {
-    $curp = json_decode($students[$i]['curpDrive'], true);
-    $sheet->setCellValue("A" . ($i + 2), $students[$i]['controlNumber']);
-    $sheet->setCellValue("B" . ($i + 2), mb_strtoupper($students[$i]['names']));
-    $sheet->setCellValue("C" . ($i + 2), mb_strtoupper($students[$i]['lastNamePaterno']));
-    $sheet->setCellValue("D" . ($i + 2), mb_strtoupper($students[$i]['lastNameMaterno'])); 
-    $auxColumn = "E";
-    foreach ($headings as $heading) {
-        if ($heading['activityType'] == "Tarea") {
-            $data = $student->getActivityScore($heading['activityType'], "AND userId = {$students[$i]['userId']} AND activityId = {$heading['activityId']}");
-            $sheet->setCellValue("{$auxColumn}{$auxRow}", (!isset($data['homeworkId'])  ? "NO ENTREGÓ" : $data['countUpdate'])); 
+    $auxRow = 2;
+    for ($i = 0; $i < (count($students)); $i++) {
+        $curp = json_decode($students[$i]['curpDrive'], true);
+        $sheet->setCellValue("A" . ($i + 2), $students[$i]['controlNumber']);
+        $sheet->setCellValue("B" . ($i + 2), mb_strtoupper($students[$i]['names']));
+        $sheet->setCellValue("C" . ($i + 2), mb_strtoupper($students[$i]['lastNamePaterno']));
+        $sheet->setCellValue("D" . ($i + 2), mb_strtoupper($students[$i]['lastNameMaterno'])); 
+        $auxColumn = "E";
+        foreach ($headings as $heading) {
+            if ($heading['activityType'] == "Tarea") {
+                $data = $student->getActivityScore($heading['activityType'], "AND userId = {$students[$i]['userId']} AND activityId = {$heading['activityId']}");
+                $sheet->setCellValue("{$auxColumn}{$auxRow}", (!isset($data['homeworkId'])  ? "NO ENTREGÓ" : $data['countUpdate'])); 
+            }
+            if ($heading['activityType'] == "Examen") {
+                $data = $student->getActivityScore($heading['activityType'], "AND userId = {$students[$i]['userId']} AND activityId = {$heading['activityId']}");
+                $sheet->setCellValue("{$auxColumn}{$auxRow}", ($data ? $data['try'] : 0));
+            }
+            $auxColumn++;
         }
-        if ($heading['activityType'] == "Examen") {
-            $data = $student->getActivityScore($heading['activityType'], "AND userId = {$students[$i]['userId']} AND activityId = {$heading['activityId']}");
-            $sheet->setCellValue("{$auxColumn}{$auxRow}", ($data ? $data['try'] : 0));
-        }
-        $auxColumn++;
+        $auxRow++;
     }
-    $auxRow++;
-}
 
-$sheet->getStyle("A2:$auxHeading" . (count($students) + 1))->getAlignment()->setHorizontal('center')->setVertical('center')->setWrapText(true);
+    $sheet->getStyle("A2:$auxHeading" . (count($students) + 1))->getAlignment()->setHorizontal('center')->setVertical('center')->setWrapText(true);
+    if ($ultimo_indice !== $key) {
+        $spreadsheet->createSheet();
+        $spreadsheet->setActiveSheetIndex($key + 1);
+    }
+}
 
 $fileName = bin2hex(random_bytes(4));
 // Redirect output to a client’s web browser (Xls)
