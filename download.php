ddcab7a7d0348a80f59469d2c06cb9effab0fda0
William Ramírez <wramirez@iapchiapas.edu.mx>
2024-09-18 16:03:55 -0600

    Fix descarga

--- download.php
+++ download.php
@@ -1,5 +1,5 @@
 <?php
 header('Content-disposition: attachment; filename='.$_GET["file"]);
 header('Content-type:application/force-download');
-readfile(WEB_ROOT."/homework/".$_GET["file"]);
+readfile(urldecode("./".$_GET["file"]));
 ?>
