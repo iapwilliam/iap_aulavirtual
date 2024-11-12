adc867e3d1c4ecfd5b68ab555f0f6e19a9285003
William Ramírez <wramirez@iapchiapas.edu.mx>
2024-11-12 12:52:27 -0600

    Si el token no existe, manda vista de error

--- modules/verificar.php
+++ modules/verificar.php
@@ -19,20 +19,22 @@ if ($dataCourse['token'] != "") {
             $course->setCourseId(7);
             $diploma['curso'][] = $course->getCourse();
         }
-    } 
+    }
 } else {
     $sql = "SELECT * FROM diplomas_alumnos WHERE token = '{$_GET['token']}'";
     $util->DB()->setQuery($sql);
     $infoDiploma = $util->DB()->GetRow();
-    $student->setUserId($infoDiploma['alumno_id']);
-    $diploma['alumno'] = $student->GetInfo();
+    if ($infoDiploma) {
+        $student->setUserId($infoDiploma['alumno_id']);
+        $diploma['alumno'] = $student->GetInfo();
 
-    $sql = "SELECT * FROM diplomas_cursos WHERE diploma_id = {$infoDiploma['diploma_id']}";
-    $util->DB()->setQuery($sql);
-    $cursos = $util->DB()->GetResult();
-    foreach ($cursos as $curso) {
-        $course->setCourseId($curso['course_id']);
-        $diploma['curso'][] =  $course->getCourse();
+        $sql = "SELECT * FROM diplomas_cursos WHERE diploma_id = {$infoDiploma['diploma_id']}";
+        $util->DB()->setQuery($sql);
+        $cursos = $util->DB()->GetResult();
+        foreach ($cursos as $curso) {
+            $course->setCourseId($curso['course_id']);
+            $diploma['curso'][] =  $course->getCourse();
+        }
     }
 }
 
