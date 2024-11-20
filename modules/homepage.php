58fdc71b0857ca4d6c1df7c89fd617a6dbc5d298
William Ramírez <wramirez@iapchiapas.edu.mx>
2024-11-20 09:46:59 -0600

    Fix cursos finalizados

--- modules/homepage.php
+++ modules/homepage.php
@@ -1,22 +1,21 @@
 <?php
-	if ($_GET['id']!=NULL)
-	{
-		$announcement->setAnnouncementId($_GET['id']);
-   		$announcement->Delete();
-	}  
-	$smarty->assign("id",$_SESSION["User"]["userId"]);  
-	$fechaFinalizacion = "DATE_FORMAT(NOW(),'%Y-%m-%d')";
-	if (in_array($_SESSION['User']['userId'], [3462, 3463, 3464]) && in_array($value['courseId'], [2,11,12,15,16])) {
-		$fechaFinalizacion = "2024-11-30";
+if ($_GET['id'] != NULL) {
+	$announcement->setAnnouncementId($_GET['id']);
+	$announcement->Delete();
+}
+$smarty->assign("id", $_SESSION["User"]["userId"]);
+$activeCourses = $student->getCourses("AND course.finalDate >= DATE_FORMAT(NOW(),'%Y-%m-%d') AND user_subject.status = 'activo' AND user_subject.alumnoId = {$_SESSION["User"]["userId"]}");
+$inactiveCourses = $student->getCourses("AND user_subject.status = 'inactivo' AND user_subject.alumnoId = {$_SESSION["User"]["userId"]}");
+$finishedCourses = $student->getCourses("AND course.finalDate < DATE_FORMAT(NOW(),'%Y-%m-%d') AND user_subject.status = 'activo' AND user_subject.alumnoId = {$_SESSION["User"]["userId"]}");
+
+foreach ($finishedCourses as $key => $value) {
+	$finishedCourses[$key]['diploma'] = $student->getDiplomas($_SESSION["User"]["userId"], $value['courseId']);
+	if (in_array($_SESSION['User']['userId'], [3462, 3463, 3464]) && date('Y-m-d') < "2024-11-30") {
+		$activeCourses[] = $finishedCourses[$key];
+		unset($finishedCourses[$key]);
 	}
-	$activeCourses = $student->getCourses("AND course.finalDate >=  $fechaFinalizacion AND user_subject.status = 'activo' AND user_subject.alumnoId = {$_SESSION["User"]["userId"]}"); 
-	$inactiveCourses = $student->getCourses("AND user_subject.status = 'inactivo' AND user_subject.alumnoId = {$_SESSION["User"]["userId"]}");
-	$finishedCourses = $student->getCourses("AND course.finalDate < DATE_FORMAT(NOW(),'%Y-%m-%d') AND user_subject.status = 'activo' AND user_subject.alumnoId = {$_SESSION["User"]["userId"]}");
-	
-	foreach ($finishedCourses as $key => $value) { 
-		$finishedCourses[$key]['diploma'] = $student->getDiplomas($_SESSION["User"]["userId"], $value['courseId']);
-	}
-	
-	$smarty->assign("activeCourses", $activeCourses); 
-	$smarty->assign("inactiveCourses", $inactiveCourses);
-	$smarty->assign("finishedCourses", $finishedCourses); 
+}
+
+$smarty->assign("activeCourses", $activeCourses);
+$smarty->assign("inactiveCourses", $inactiveCourses);
+$smarty->assign("finishedCourses", $finishedCourses);
