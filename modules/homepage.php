89b4f5fc9b02888072517c60f5f144247f40eb80
William Ramírez <wramirez@iapchiapas.edu.mx>
2024-11-20 09:30:13 -0600

    Se le amplia el tiempo a ciertos alumnos en cursos específicos

--- modules/homepage.php
+++ modules/homepage.php
@@ -10,6 +10,9 @@
 	$finishedCourses = $student->getCourses("AND course.finalDate < DATE_FORMAT(NOW(),'%Y-%m-%d') AND user_subject.status = 'activo' AND user_subject.alumnoId = {$_SESSION["User"]["userId"]}");
 	
 	foreach ($finishedCourses as $key => $value) {
+		if (in_array($_SESSION['User']['userId'], [3462, 3463, 3464]) && in_array($value['courseId'], [2,11,12,15,16])) {
+			$finishedCourses[$key]['finalDate'] = "2029-11-29";
+		}
 		$finishedCourses[$key]['diploma'] = $student->getDiplomas($_SESSION["User"]["userId"], $value['courseId']);
 	}
 	
