007e2a70098c72ec0314b1b5b8885a1f33e3e605
William Ramírez <wramirez@iapchiapas.edu.mx>
2025-02-28 15:57:27 -0600

    Se cambió el formato de la fecha

--- modules/student-curricula.php
+++ modules/student-curricula.php
@@ -1,7 +1,7 @@
 <?php
 $student->setUserId($_GET["id"]);
-$activeCourses = $course->getCourses("AND course.finalDate >= NOW()");
-$smarty->assign('activeCourses', $activeCourses); 
+$activeCourses = $course->getCourses("AND course.finalDate >= '" . date('Y-m-d') . "' ");
+$smarty->assign('activeCourses', $activeCourses);
 $activeCoursesStudent = $student->getCourses("AND user_subject.alumnoId = {$_GET['id']}");
 $smarty->assign("activeCourseStudent", $activeCoursesStudent);
 $smarty->assign("student", $_GET['id']);
